4d8f3cc8dfeb9f7de25cb696915be2fe219a12c3
karan sonwane <karansonwane49@gmail.com>
2024-09-03 17:29:02 +0530

    main home page added

--- src/components/routingcomponent/routing.js
+++ src/components/routingcomponent/routing.js
@@ -7,10 +7,12 @@ import Homepage from '../HomePage_after_login/Homepage'
 export default function routing() {
   return (
   <>
+  <h1>hello</h1>
   <BrowserRouter>
    <Routes>
     {/* <Route path="/" element={<Homepage />} /> */}
       <Route path='/' element={<Home/>}></Route>
+      
       <Route path='Login' element={<Login/>}></Route>
       <Route path='Signup' element={<Signup/>}></Route>
    </Routes>
